f275379ed1dcfcd05c37b6edb8532eb2879ae51e
ricardosmbr <ricardosm@rede.ulbra.br>
2019-10-30 11:27:37 -0300

    ajustes!

--- apps/registro_hora_extra/models.py
+++ apps/registro_hora_extra/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from apps.funcionarios.models import Funcionario
 
 # Create your models here.
 class Registro_hora_extra(models.Model):
     motivo = models.CharField(max_length=100)
-
+    hora_extra = models.ForeignKey(Funcionario, on_delete=models.PROTECT)
     def __str__(self):
         return self.motivo
 
